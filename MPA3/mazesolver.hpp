cc4388b447b73958d76f96dec1d7546213dcf50d
Justin Po <justin.andre.po@gmail.com>
2019-10-31 18:29:21 +0800

    added mazesolver constructor

--- MPA3/mazesolver.hpp
+++ MPA3/mazesolver.hpp
@@ -1,10 +1,21 @@
-class Mazesolver{
+#include "maze.hpp"
+#include "agenda.hpp"
+
+template <class T>
+class Mazesolver {
 private:
-    Agenda *a;
-    Maze *m;
+    Agenda<T> *_agenda;
+    Maze *_maze;
 public:
     Mazesolver();
+    Mazesolver(Maze);
     ~Mazesolver();
     void solve();
     void writeSolution();
 };
+
+template <class T>
+Mazesolver::Mazesolver(Maze maze) {
+    _agenda = new Agenda<T>();
+    _maze = maze;
+}
